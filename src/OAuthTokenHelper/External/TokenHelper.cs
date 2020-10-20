ea83de03e01bc37665d461c33cd18c5defdd22d9
Wael Alissa <wael.alissa@ibiz-solutions.se>
2020-10-20 16:36:03 +0200

    making claims optional

---
+++ src/OAuthTokenHelper/External/TokenHelper.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace BizTalkComponents.CustomComponents.OAuthTokenHelper
+{
+    [Serializable]
+    public static class TokenHelper
+    {
+        public static string GetToken(string oAuth2Url, string client_Id, string client_Secret, string claims, bool forceNewToken = false)
+        {
+            return AppDomainHelper.TokenDictionary.GetToken(oAuth2Url, client_Id, client_Secret, claims, forceNewToken);
+        }
+    }
+}
